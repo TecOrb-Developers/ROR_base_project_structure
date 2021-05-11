ed4b9aa59ed5c0f4709f7a9a6f4b778e720af1af
Jai Rajput <jai@tecorb.co>
2021-05-12 00:15:26 +0530

    Page not found configurations added to application controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
   require 'sidekiq/cron/web'
   mount Sidekiq::Web => '/sidekiq'
-  
+
+  match "*a" => "application#notFound", via: :all
+
 end
